3e1060a4c9c933e1fe912c82c463ee1eee64ffe3
ksenialukova <ksushalukova778@gmail.com>
2025-08-10 17:17:40 +0200

    Refactor

--- index.js
+++ index.js
@@ -12,6 +12,39 @@ app.get('/index', (req, res) => {
   res.json('Hello! I am Take Taro!');
 });
 
+app.get("/install", async (req, res) => {
+  const code = req.query.code;
+  if (!code) {
+    return res.status(400).send("No code provided");
+  }
+
+  try {
+    const params = new URLSearchParams();
+    params.append("client_id", process.env.SLACK_CLIENT_ID);
+    params.append("client_secret", process.env.SLACK_CLIENT_SECRET);
+    params.append("code", code);
+    params.append("redirect_uri", process.env.SLACK_REDIRECT_URI);
+
+    const response = await fetch("https://slack.com/api/oauth.v2.access", {
+      method: "POST",
+      body: params
+    });
+
+    const data = await response.json();
+
+    if (!data.ok) {
+      return res.status(400).send(`OAuth failed: ${data.error}`);
+    }
+
+    console.log("Access Token:", data.access_token);
+
+    res.send("You authorised succesfully. You can close this tab.");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
 app.post('/slack/taketaro', (req, res) => {
   console.log('Start GET /slack/taketaro');
   const randomNumber = Math.floor(Math.random() * 78);
