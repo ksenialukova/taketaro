5e0ea17dec4d5840a9c35c52ce454dd7f15f530f
ksenialukova <ksushalukova778@gmail.com>
2025-08-10 17:58:39 +0200

    Refactor

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/index', (req, res) => {
-  res.json('Hello! I am Take Taro!');
+  res.json('Hello! I am Take Taro!/n<a href="https://slack.com/oauth/v2/authorize?client_id=7980516449653.7976907411302&scope=commands&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
 });
 
 app.get('/policy', (req, res) => {
